66d10f6ea830eb87a2f83f83a835f01682e90e30
Maria Mussi <mariamussi@id.uff.br>
2019-11-01 10:54:47 -0300

    Inicialização da arvore B

---
+++ TARVB.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int t = 2;
+
+typedef struct ArvB{
+  int nchaves, folha, *chave;
+  struct ArvB **filho;
+}TAB;
+
+TAB *inicializa();
+//Cria um nó na arvore B (sempre folha e com ponteiros de filho == NULL)
+TAB *cria(int t);
+//Imprime a arvore B
+void imprime(TAB *a, int andar);
+//Libera a arvore B
+TAB *libera(TAB *a);
+//Busca nó que contém/deveria conter a chave X
+TAB *busca(TAB* x, int ch);
+//Insere chave k na arvore de ordem t
+TAB *insere(TAB *T, int k, int t);
+TAB* remover(TAB* arv, int ch, int t);
+TAB* retira(TAB* arv, int k, int t);
