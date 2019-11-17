e73b99d87a30ccc22591c1b765aa53d208549cfd
jmsgfhr <jonathanms30@gmail.com>
2019-11-17 20:26:16 -0300

    feito uma lista encadeada do alfabeto para iniciar a compressao

--- huffman.c
+++ huffman.c
@@ -8,6 +8,13 @@ typedef struct huffman
     float frequencia;
 }Huff;
 
+typedef struct LHuffman
+{
+    Huff letraFreq;
+    struct LHuffman *prox;
+}ListHuff;
+
+
 
 int main(int argc, char *argv[]){
     /*
@@ -17,7 +24,7 @@ int main(int argc, char *argv[]){
     char letr;
     Huff x;
 
-    ------------------ Escreve no arquivo binario ------------------
+    //------------------ Escreve no arquivo binario ------------------
     fp=fopen("alfabetoFreq.bin","wb"); //abre arquivo
     scanf("%c %f\n",&letr,&freq);
     while (freq>=0)
@@ -32,7 +39,7 @@ int main(int argc, char *argv[]){
         scanf("%c %f\n",&letr,&freq);
     }
     
-    ------------------ Le do arquivo binario ------------------
+    //------------------ Le do arquivo binario ------------------
     fp=fopen("alfabetoFreq.bin","rb");
     while (!feof(fp)) // verifica se chegou ao fim do arquivo 
     {
@@ -42,4 +49,32 @@ int main(int argc, char *argv[]){
     
     fclose(fp);
     */
+
+
+    //------------------ Cria uma lista encadeada (desnecessaria) para ordenar e comprimir a arvore ------------------
+    FILE *fp;
+    float freq;
+    char letr;
+    ListHuff *listLetra = NULL; //variavel inicial da lista
+    //------------------ Le do arquivo binario ------------------
+    fp=fopen("alfabetoFreq.bin","rb");
+    while (!feof(fp)) // verifica se chegou ao fim do arquivo 
+    {
+        Huff x; //estrutura do no
+        fread(&x,sizeof(x),1,fp); //le uma estrutura por vez
+        ListHuff *aux = (ListHuff *) malloc(sizeof(ListHuff)); //alocando o no
+        aux->prox = listLetra;
+        aux->letraFreq = x;
+        listLetra = aux;
+    }
+    while (listLetra!=NULL)
+    {
+        printf("%c %f\n",listLetra->letraFreq.letra, listLetra->letraFreq.frequencia);
+        ListHuff *aux = listLetra;
+        listLetra = listLetra->prox;
+        free(aux);
+    }
+    
+    
+    fclose(fp);
 }
