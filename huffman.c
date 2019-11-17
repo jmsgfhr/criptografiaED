eacc102eea170d7fc8ef4eff8f6001fd03a80e8e
jmsgfhr <jonathanms30@gmail.com>
2019-11-17 19:39:42 -0300

    Criado a Tabela de letras e suas frequencias em arquivo binario persistente (errata com comentarios)

--- huffman.c
+++ huffman.c
@@ -18,25 +18,25 @@ int main(int argc, char *argv[]){
     Huff x;
 
     ------------------ Escreve no arquivo binario ------------------
-    fp=fopen("alfabetoFreq.bin","wb");
+    fp=fopen("alfabetoFreq.bin","wb"); //abre arquivo
     scanf("%c %f\n",&letr,&freq);
     while (freq>=0)
     {
-        fflush(stdin);
+        fflush(stdin); //limpa buffer do teclado
         if(freq>=0){
             x.letra = letr;
             x.frequencia = freq;
-            printf("letra: %c  Freq: %f\n",x.letra,x.frequencia);
-            fwrite(&x,sizeof(x),1,fp);
+            printf("letra: %c  Freq: %f\n",x.letra,x.frequencia); //teste de leitura
+            fwrite(&x,sizeof(x),1,fp); // escreve estrutura em arquivo
         }
         scanf("%c %f\n",&letr,&freq);
     }
     
     ------------------ Le do arquivo binario ------------------
     fp=fopen("alfabetoFreq.bin","rb");
-    while (!feof(fp))
+    while (!feof(fp)) // verifica se chegou ao fim do arquivo 
     {
-        fread(&x,sizeof(x),1,fp);
+        fread(&x,sizeof(x),1,fp); //le uma estrutura por vez
         printf("%c %f\n",x.letra,x.frequencia);
     }
     
